34a6a09d88b5ca045c0b6651f72c1f0aab1696cf
Динар Калимуллин <di.kalimullin@bars-open.ru>
2023-02-05 19:44:07 +0300

    test

---
+++ quox-backend/IncService.cs
@@ -0,0 +1,7 @@
+namespace quox_backend;
+
+public class IncService
+{
+    private int count = 0;
+    public int Count => count++;
+}
--- quox-backend/Program.cs
+++ quox-backend/Program.cs
@@ -1,3 +1,5 @@
+using quox_backend;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IncService>();
 
 var app = builder.Build();
 
